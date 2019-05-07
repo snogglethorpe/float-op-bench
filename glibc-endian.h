d3ad7eff9510475f01e6428ced825276cc8e8525
Miles Bader <snogglethorpe@gmail.com>
2019-05-07 17:41:06 -0500

    glibc-endian.h: Handle undefined __FLOAT_WORD_ORDER__

--- glibc-endian.h
+++ glibc-endian.h
@@ -1,12 +1,12 @@
 #include <endian.h>
 
-# if __FLOAT_WORD_ORDER == __BIG_ENDIAN
+# if __FLOAT_WORD_ORDER__ == __ORDER_BIG_ENDIAN__ || (!__FLOAT_WORD_ORDER__ && (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__))
 #  define BIG_ENDI 1
 #  undef LITTLE_ENDI
 #  define HIGH_HALF 0
 #  define  LOW_HALF 1
 # else
-#  if __FLOAT_WORD_ORDER == __LITTLE_ENDIAN
+#  if __FLOAT_WORD_ORDER__ == __ORDER_LITTLE_ENDIAN__ || (!__FLOAT_WORD_ORDER__ && (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__))
 #   undef BIG_ENDI
 #   define LITTLE_ENDI 1
 #   define HIGH_HALF 1
